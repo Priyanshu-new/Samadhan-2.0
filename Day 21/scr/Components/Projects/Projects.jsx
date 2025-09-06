3c55b178c0539838dfed5d71428a4c95c41f62d8
Priyanshu-new <samadhan22222222@gmail.com>
2025-09-06 18:16:57 +0530

    Add files via upload

---
+++ Day 21/scr/Components/Projects/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const Projects = () => {
+  return (
+    <div id="Projects" className="p-10 md:p-24 text-white ">
+      <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
+      <div className="py-12 px-8 flex flex-wrap gap-5">
+        <ProjectCard
+          title="Utsav Collection"
+          main="this is a e-commerce website"
+        ></ProjectCard>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
